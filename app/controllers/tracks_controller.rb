403eafde5dafb9eb9240fcb397021d6310468fd3
Christina <christina.v.stewart@gmail.com>
2014-04-23 12:44:13 -0400

    latest

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -4,7 +4,7 @@ def result
 	@playlist = Playlist.new
 	@query = params["track"]["url"]
 
-	@client = Soundcloud.new(:client_id => 'd934a1c90d9d16288578bac5d6800e8e', 
+	@client = Soundcloud.new(:client_id => 'd934a1c90d9d16288578bac5d6800e8e',
   		:client_secret => '23d8c41c38328fc9e4a138789977ba0a')
 	@tracks = @client.get('/tracks', :q => @query)
 	render "result"
